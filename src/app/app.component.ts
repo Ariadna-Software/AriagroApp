a19793e675803a5449c2f00221f4ed8b92bfdc05
unknown <vicent@myariadna.com>
2018-06-27 12:31:16 +0200

    plugin OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Locales } from '../locales/locales';
+import { OneSignal, OSNotificationPayload } from '../providers/onesignal';
 
 @Component({
   templateUrl: 'app.html'
@@ -10,16 +11,46 @@ import { Locales } from '../locales/locales';
 export class MyApp {
   rootPage: any = 'HomePage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, oneSignal: OneSignal ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // OneSignal Code start:
+      // Enable to debug issues:
+      // window["plugins"].OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+      var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+     
+     
+    
+      
+      oneSignal.startInit("33728f44-2576-4f76-9b7c-b6a65d345c28", "595606821946");
+      oneSignal.handleNotificationOpened();
+
+      oneSignal.inFocusDisplaying(oneSignal.OSInFocusDisplayOption.Notification);
+      oneSignal.handleNotificationReceived().subscribe(data => this.onPushReceived(data.payload));
+      oneSignal.handleNotificationOpened().subscribe(data => this.onPushOpened(data.notification.payload));
+
+      oneSignal.endInit();
+
       // Setting locales for the entire app
       let loc = new Locales();
       loc.NumeralLocales();
     });
+  }
+
+  private onPushReceived(payload: OSNotificationPayload) {
+    alert( payload.body);
+  }
+  
+  private onPushOpened(payload: OSNotificationPayload) {
+    alert(payload.body);
   }
 }
 

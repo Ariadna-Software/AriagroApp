1cf8dc14d9f7777bb59411e116c6779bd8d62f6a
Rafael Garcia <rafa@myariadna.com>
2018-07-10 09:50:31 +0200

    cambio login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -114,6 +114,8 @@ export class LoginPage {
                             (error) => {
                               if (error.status == 404) {
                                 this.showAlert("AVISO", "Usuario o contraseña incorrectos");
+                              } else {
+                                this.showAlert("ERROR", JSON.stringify(error, null, 4));
                               }
                             });
                             this.oneSignal.endInit();

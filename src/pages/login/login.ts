83ef6b1ca080a52a79a1969d2a92d90127a279bd
Rafael Garcia <rafa@myariadna.com>
2018-07-10 09:53:50 +0200

    cambio login2

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -20,7 +20,7 @@ export class LoginPage {
   password: string = "";
   nombreCooperativa: string = "LA COOPPP";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public plt: Platform, public loadingCtrl: LoadingController, 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public plt: Platform, public loadingCtrl: LoadingController,
     public formBuilder: FormBuilder, public alertCrtl: AlertController, public viewCtrl: ViewController,
     public ariagroData: AriagroDataProvider, public localData: LocalDataProvider, public oneSignal: OneSignal) {
     this.loginForm = formBuilder.group({
@@ -68,70 +68,70 @@ export class LoginPage {
     }
   }
 
- 
+
 
   pushUser(user) {
-        this.plt.ready().then( () => {
-            // obtener los parámetros
-        let loading = this.loadingCtrl.create({ content: 'Buscando mensajes...' });
-        loading.present();
-        this.ariagroData.getParametros(this.settings.parametros.url)
+    this.plt.ready().then(() => {
+      // obtener los parámetros
+      let loading = this.loadingCtrl.create({ content: 'Buscando mensajes...' });
+      loading.present();
+      this.ariagroData.getParametros(this.settings.parametros.url)
         .subscribe(
           (data) => {
-              loading.dismiss();
-              
-              var config = this.settings;
-              config.paramPush = data
-              
-              this.localData.saveSettings(config)
+            loading.dismiss();
 
-              var notificationOpenedCallback = function(jsonData) {
-                alert("NOTIFICA LOGIN:\n" + JSON.stringify(jsonData));
-                console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+            var config = this.settings;
+            config.paramPush = data
+
+            this.localData.saveSettings(config)
+
+            var notificationOpenedCallback = function (jsonData) {
+              alert("NOTIFICA LOGIN:\n" + JSON.stringify(jsonData));
+              console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
             };
 
-              
-            try{
+
+            try {
               // Registro OneSignal
-                            this.oneSignal.startInit(config.paramPush.appId,  config.paramPush.gcm );
-                            this.oneSignal.getIds().then( (ids) =>{
-                              var myUser = this.settings.user;
-                                
-                                //alert(JSON.stringify(ids));
-                                if(config.user.playerId != ids.userId) {
-                                 
-                                  myUser.playerId = ids.userId;
-                                  this.ariagroData.putUsuario(this.settings.parametros.url, myUser.usuarioPushId ,myUser)
-                                  .subscribe((data) => {
-                                      this.settings.user = data;
-                                  },
-                                  (err) => {
-                                    loading.dismiss();
-                                    console.log("PUT usuario error");
-                                  }); 
-                                }
-                            },
-                            (error) => {
-                              if (error.status == 404) {
-                                this.showAlert("AVISO", "Usuario o contraseña incorrectos");
-                              } else {
-                                this.showAlert("ERROR", JSON.stringify(error, null, 4));
-                              }
-                            });
-                            this.oneSignal.endInit();
-            }  catch(e) {
+              this.oneSignal.startInit(config.paramPush.appId, config.paramPush.gcm);
+              this.oneSignal.getIds().then((ids) => {
+                var myUser = this.settings.user;
+
+                //alert(JSON.stringify(ids));
+                if (config.user.playerId != ids.userId) {
+
+                  myUser.playerId = ids.userId;
+                  this.ariagroData.putUsuario(this.settings.parametros.url, myUser.usuarioPushId, myUser)
+                    .subscribe((data) => {
+                      this.settings.user = data;
+                    },
+                      (err) => {
+                        loading.dismiss();
+                        console.log("PUT usuario error");
+                      });
+                }
+              },
+                (error) => {
+                  if (error.status == 404) {
+                    this.showAlert("AVISO", "Usuario o contraseña incorrectos");
+                  } else {
+                    this.showAlert("ERROR", JSON.stringify(error, null, 4));
+                  }
+                });
+              this.oneSignal.endInit();
+            } catch (e) {
               console.log("Error de carga de oneSignal");
             }
-              
+
           },
           (err) => {
             loading.dismiss();
             if (err) {
-                var msg = err || err.message;
-                this.showAlert("ERROR", msg);
-            } 
+              var msg = err || err.message;
+              this.showAlert("ERROR", msg);
+            }
           });
-        });
+    });
   }
 
 
@@ -152,6 +152,6 @@ export class LoginPage {
     alert.present();
   }
 
-  
+
 
 }

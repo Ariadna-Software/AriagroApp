f3b047479aff92cdbd2d8b4fcb5e61d6f539a360
unknown <vicent@myariadna.com>
2018-10-24 13:11:59 +0200

    OK

--- src/providers/ariagro-data/ariagro-data.ts
+++ src/providers/ariagro-data/ariagro-data.ts
@@ -160,4 +160,37 @@ enviarCorreoFactu(url, numfactu, email, ruta, coop, codtipom): any {
   };
   return this.http.post(url + '/api/anticipos-liquidaciones/enviar/correo/ant-liq', data);
 }
+prepararCorreoFactuGasol(url, fecfactu, numfactu, letraser, informe, codclien): any {
+  var data = {
+    fecfactu: fecfactu,
+    numfactu: numfactu,
+    letraser: letraser,
+    informe: informe,
+    codclien: codclien
+};
+return this.http.post(url + '/api/facturas/gasolinera/correo', data);
+}
+
+prepararCorreoFactuTienda(url, fecfactu, numfactu, letraser, informe, codclien): any {
+  var data = {
+    fecfactu: fecfactu,
+    numfactu: numfactu,
+    letraser: letraser,
+    informe: informe,
+    codclien: codclien
+};
+return this.http.post(url + '/api/facturas/tienda/correo/esto', data);
+}
+
+enviarCorreoFactuComun(url, numfactu, email, ruta, coop, codtipom): any {
+  var data = {
+    numfactu: numfactu,
+    email: email,
+    ruta: ruta,
+    coop: coop,
+    codtipom: codtipom
+  };
+  return this.http.post(url + '/api/facturas/enviar/correo/comun', data);
+}
+
 }
